f1fe966377dec224026cf8d8c0345b8f6d221a8c
Todd Broch <tbroch@chromium.org>
2011-07-29 12:43:23 -0700

    hdctools: dut-control: add repeat (-r, -t) and statistics
    Added switches to repeat command.
      -r REPEAT, --repeat=REPEAT
                            repeat requested command multiple times
      -t TIME_IN_SECS, --time_in_secs=TIME_IN_SECS
                            repeat requested command forthis many seconds
    
    When activated it will also print stats if 'get' is numeric
    
    @@            NAME  COUNT  AVERAGE  STDDEV   MAX   MIN
    @@    sample_msecs    400     5.00    0.21     8     4
    @@   ppvar_vbat_ma    400  3586.92   79.78  3952  3443
    
    BUG=none
    TEST=manual,
    
    prereq:
    -------
    servod -c servo.xml -c servo_loc.xml
    dut-control i2c_mux_en:on i2c_mux:loc0
    
    dut-control -r 100 loc_0x40_mv
    dut-control -t 2 loc_0x40_mv
    
    Change-Id: Idab2bdae9fe74a4925f52f2491badde7445e14cc
    Reviewed-on: http://gerrit.chromium.org/gerrit/5018
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

--- servo/dut_control.py
+++ servo/dut_control.py
@@ -4,14 +4,22 @@
 # found in the LICENSE file.
 """Client to control DUT hardware connected to servo debug board
 """
+import collections
 import logging
+import numpy
 import optparse
+import sys
+import time
 import xmlrpclib
 
 
 # TODO(tbroch) determine version string methodology.
 VERSION = "0.0.1"
 
+# used to aide sorting of dict keys
+KEY_PREFIX='__'
+# dict key for tracking sampling time
+TIME_KEY = KEY_PREFIX + 'sample_msecs'
 
 class ServoClient(object):
   """Class to link client to servod via xmlrpc.
@@ -73,6 +81,9 @@ def _parse_args():
     "   %prog\n\tgets value for all controls\n"
     "   %prog -v\n\tgets value for all controls verbosely\n"
     "   %prog i2c_mux\n\tgets value for 'i2c_mux' control\n"
+    "   %prog -r 100 i2c_mux\n\tgets value for 'i2c_mux' control 100 times\n"
+    "   %prog -t 2 loc_0x40_mv\n\tgets value for 'loc_0x40_mv' control for 2 "
+    "seconds\n"
     "   %prog -v i2c_mux\n\tgets value for 'i2c_mux' control verbosely\n"
     "   %prog i2c_mux:remote_adcs\n\tsets 'i2c_mux' to value 'remote_adcs'\n"
     )
@@ -86,11 +97,104 @@ def _parse_args():
                     action="store_true", default=False)
   parser.add_option("-i", "--info", help="show info about controls",
                     action="store_true", default=False)
+  parser.add_option("-r", "--repeat", type=int,
+                    help="repeat requested command multiple times", default=1)
+  parser.add_option("-t", "--time_in_secs", help="repeat requested command for"
+                    + "this many seconds", type='float', default=0.0)
   parser.add_option("-d", "--debug", help="enable debug messages",
                     action="store_true", default=False)
 
   parser.set_usage(parser.get_usage() + examples)
   return parser.parse_args()
+
+def display_table(table, prefix='@@'):
+  """Display a two-dimensional array ( list-of-lists ) as a table.
+
+  The table will be spaced out.
+  >>> table = [['aaa', 'bbb'], ['1', '2222']]
+  >>> display_table(table)
+  @@   aaa   bbb
+  @@     1  2222
+  >>> display_table(table, prefix='%')
+  %   aaa   bbb
+  %     1  2222
+  >>> table = [['a']]
+  >>> display_table(table)
+  @@   a
+  >>> table = []
+  >>> display_table(table)
+  >>> table = [[]]
+  >>> display_table(table)
+  >>> table = [['a'], ['1', '2']]
+  >>> display_table(table)
+  Traceback (most recent call last):
+  ...
+  IndexError: list index out of range
+  >>> table = [['a', 'b'], ['1']]
+  >>> display_table(table)
+  Traceback (most recent call last):
+  ...
+  IndexError: list index out of range
+  >>> table = [['aaa', 'bbb', 'c'], ['1', '2222', '0']]
+  >>> display_table(table)
+  @@   aaa   bbb  c
+  @@     1  2222  0
+
+  Args:
+    table: A two-dimensional array (list of lists) to show.
+    prefix: All lines will be prefixed with this and a space.
+  """
+  if len(table) == 0 or len(table[0]) == 0:
+    return
+
+  max_col_width = []
+  for col_idx in xrange(len(table[0])):
+    col_item_widths = [len(row[col_idx]) for row in table]
+    max_col_width.append(max(col_item_widths))
+
+  for row in table:
+    out_str = ''
+    for i in xrange(len(row)):
+      out_str += row[i].rjust(max_col_width[i] + 2)
+    print prefix, out_str
+
+def display_stats(stats):
+  """Display various statistics for data captured in a table.
+  >>> stats = {}
+  >>> stats[TIME_KEY] = [50.0, 25.0, 40.0, 10.0]
+  >>> stats['frobnicate'] = [11.5, 9.0]
+  >>> stats['foobar'] = [11111.0, 22222.0]
+  >>> display_stats(stats)
+  @@           NAME  COUNT   AVERAGE   STDDEV       MAX       MIN
+  @@   sample_msecs      4     31.25    15.16     50.00     10.00
+  @@         foobar      2  16666.50  5555.50  22222.00  11111.00
+  @@     frobnicate      2     10.25     1.25     11.50      9.00
+
+  Args:
+    stats: A dictionary of stats to show.  Key is name of result and value is a
+        list of floating point values to show stats for.  See doctest.
+        Any key starting with '__' will be sorted first and have its prefix
+        stripped.
+  """
+  table = [['NAME', 'COUNT', 'AVERAGE', 'STDDEV', 'MAX', 'MIN']]
+  for key in sorted(stats.keys()):
+    if stats[key]:
+      stats_np = numpy.array(stats[key])
+      disp_key = key.lstrip(KEY_PREFIX)
+      row = [disp_key, str(len(stats_np))]
+      row.append("%.2f" % stats_np.mean())
+      row.append("%.2f" % stats_np.std())
+      row.append("%.2f" % stats_np.max())
+      row.append("%.2f" % stats_np.min())
+      table.append(row)
+  display_table(table)
+
+def timed_loop(time_in_secs):
+  start_time = time.time()
+  secs_so_far = 0.0
+  while secs_so_far <= time_in_secs:
+    yield secs_so_far
+    secs_so_far = time.time() - start_time
 
 def main():
   (options, args) = _parse_args()
@@ -108,40 +212,58 @@ def main():
     print sclient.doc_all()
   elif not len(args):
     print sclient.get_all(verbose=options.verbose)
-  for argnum, arg in enumerate(args):
-    logging.debug("cmd = %s" % arg)
-    cmdlist = arg.split(':', 1)
-    if len(cmdlist) == 2:
-      (cmd_name, cmd_value) = cmdlist
-      # its a set
-      try:
-        rv = sclient.set(cmd_name, cmd_value)
-        if options.verbose:
-          print "SET %s -> %s :: %s" % (cmd_name, cmd_value,
-                                        sclient.doc(cmd_name))
 
-      except xmlrpclib.Fault, e:
-        # TODO(tbroch) : more detail of failure.  Note xmlrpclib only
-        #                passes one exception above
-        logging.error("Problem setting %s :: %s\n\t\tIgnoring %s" %
-                      (arg, e, ' '.join(args[argnum + 1:])))
-        break
-    else:
-      (cmd_name,) = cmdlist
-      # its a get | doc
-      get_fx = "get"
-      if options.info:
-        get_fx = "doc"
-      try:
-        rv = eval("sclient.%s(cmd_name)" % (get_fx))
-        if options.verbose:
-          print "%s %s -> %s" % (get_fx.upper(), cmd_name, rv)
-        else:
-          print "%s:%s" % (arg, rv)
-      except  xmlrpclib.Fault, e:
-        logging.error("Problem getting %s %s :: %s\n\t\tIgnoring %s" %
-                      (get_fx, arg, e, ' '.join(args[argnum+1:])))
-        break
+  stats = collections.defaultdict(list)
+
+  if options.time_in_secs > 0:
+    iterate_over = timed_loop(options.time_in_secs)
+  else:
+    iterate_over = xrange(options.repeat)
+
+  for _ in iterate_over:
+    sample_start = time.time()
+    for argnum, arg in enumerate(args):
+      logging.debug("cmd = %s" % arg)
+      cmdlist = arg.split(':', 1)
+      if len(cmdlist) == 2:
+        (cmd_name, cmd_value) = cmdlist
+        # its a set
+        try:
+          rv = sclient.set(cmd_name, cmd_value)
+          if options.verbose:
+            print "SET %s -> %s :: %s" % (cmd_name, cmd_value,
+                                          sclient.doc(cmd_name))
+        except xmlrpclib.Fault, e:
+          # TODO(tbroch) : more detail of failure.  Note xmlrpclib only
+          #                passes one exception above
+          logging.error("Problem setting %s :: %s\n\t\tIgnoring %s" %
+                        (arg, e, ' '.join(args[argnum + 1:])))
+          break
+      else:
+        (cmd_name,) = cmdlist
+        # its a get | doc
+        get_fx = "get"
+        if options.info:
+          get_fx = "doc"
+        try:
+          rv = eval("sclient.%s(cmd_name)" % (get_fx))
+          if options.verbose:
+            print "%s %s -> %s" % (get_fx.upper(), cmd_name, rv)
+          else:
+            print "%s:%s" % (arg, rv)
+          if get_fx == 'get':
+            try:
+              stats[cmd_name].append(float(rv))
+            except ValueError, e:
+              pass
+        except  xmlrpclib.Fault, e:
+          logging.error("Problem getting %s %s :: %s\n\t\tIgnoring %s" %
+                        (get_fx, arg, e, ' '.join(args[argnum+1:])))
+          break
+        stats[TIME_KEY].append((time.time() - sample_start) * 1000)
+
+  if (options.repeat != 1) or (options.time_in_secs > 0):
+    display_stats(stats);
 
 if __name__ == '__main__':
   try:
