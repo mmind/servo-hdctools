d25e32336016c422799ffe5d5e812a3c4dc4f6ad
Todd Broch <tbroch@chromium.org>
2012-01-17 15:49:35 -0800

    hdctools: servod: Add open-drain style driver.
    Servo V2 relies on TCA6416 GPIO expanders to implement open drain (OD)
    style outputs.  This CL adds that implementation to the driver via an
    additional parameter in the controls definition.
    
         od="PU" : open drain pull-up.  When output is intended to be a
         logical '1' the driver should be changed to an input.
    
    The CL also adds these key=value parameters to the OD style outputs
    defined by the Debug Header Requirements document for servo V2.
    
    BUG=chrome-os-partner:7585
    TEST=manual, run servod and examine both OD and push-pull style
    GPIO's that are controlled via TCA6416 IC's on servo V2.
    Change-Id: I5bc6f6d6abbfba3a05cf617099fc9c1f2bcae6be
    Reviewed-on: https://gerrit.chromium.org/gerrit/14373
    Reviewed-by: Mark Hayter <mdhayter@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Commit-Ready: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

--- servo/drv/tca6416.py
+++ servo/drv/tca6416.py
@@ -40,12 +40,12 @@ class tca6416(drv.hw_driver.HwDriver):
     until a new command byte has been sent.'  However the ports act as a
     pair so the behavior is more complex.
 
-    For example if you start with a transcation like:
+    For example if you start with a transaction like:
     i2c write of 0x00 (input register of port 0) then
     i2c read of 1 byte == input register values of port 0 then
     i2c read of 1 byte == input register values of port 1 then
     i2c read of 1 byte == input register values of port 0 then
-    
+
     Effectively the reg value to cache is:
       reg_cache = (reg & 0x6) ^ (~reg & (0x1 & rcnt))
 
@@ -56,6 +56,7 @@ class tca6416(drv.hw_driver.HwDriver):
     Mandatory Params:
       slv: integer, 7-bit i2c slave address
       port: integer, either 0 || 1
+      offset: integer, gpio's bit position from lsb
     Optional Params:
     """
     super(tca6416, self).__init__(interface, params)
@@ -87,27 +88,57 @@ class tca6416(drv.hw_driver.HwDriver):
     2. Mask accordingly
     3. Write value to Output register
     4. Read Direction register (Note 0 == output, 1 == input)
-       a. if input, Write Direction register 
+       a. if input, Write Direction register
     5. Read Input register for returning
 
-    Args: 
-      fmt_value: Integer value to write to hardware.
+    Args:
+      fmt_value: Integer value to write to hardware.  If None or empty string
+        defaults to 0.
+
+    Raises:
+    Tca6416Error: If width of open drain driver is != 1 or open drain type is
+      not recognized.
     """
     self._logger.debug("")
-    hw_value = 0
-    (offset, mask) = self._get_offset_mask()
-    if fmt_value:
-      hw_value = self._create_hw_value(fmt_value)
+    (_, mask) = self._get_offset_mask()
+    if mask is None:
+        raise Tca6416Error("Unable to determine mask.  Is offset declared?")
+
+    open_drain = None
+    if 'od' in self._params:
+      width = self._params.get('width', 1)
+      if width > 1:
+        raise Tca6416Error("Open drain not implemented for widths != 1")
+      open_drain = self._params['od'].upper()
+      if open_drain != "PU":
+        raise Tca6416Error("Unrecognized open-drain %s" % open_drain)
+
+    change_to_input = False
+    if (open_drain == "PU" and fmt_value == 1):
+      self._logger.debug("Set to input because its an open-drain %s",
+                         open_drain)
+      change_to_input = True
+
     # output register handling
-    out_reg = self._i2c_obj._read_reg(self.REG_OUT + self._port)
-    self._i2c_obj._write_reg(self.REG_OUT + self._port,
-                             hw_value | (out_reg & ~mask))
+    if not change_to_input:
+      hw_value = 0
+      if fmt_value:
+        hw_value = self._create_hw_value(fmt_value)
+
+      current_out_reg = self._i2c_obj._read_reg(self.REG_OUT + self._port)
+      new_out_reg = hw_value | (current_out_reg & ~mask)
+      if new_out_reg != current_out_reg:
+        self._i2c_obj._write_reg(self.REG_OUT + self._port, new_out_reg)
 
     #TODO(tbroch) cache direction register for speedup
-    dir_reg = self._i2c_obj._read_reg(self.REG_DIR + self._port)
-    if dir_reg & mask:
-      # its presently an input ... flip dir bit(s)
-      self._i2c_obj._write_reg(self.REG_DIR + self._port, dir_reg & ~mask)
+    current_dir_reg = self._i2c_obj._read_reg(self.REG_DIR + self._port)
+    if change_to_input:
+      new_dir_reg = current_dir_reg | mask
+    else:
+      new_dir_reg = current_dir_reg & ~mask
+
+    if new_dir_reg != current_dir_reg:
+      self._i2c_obj._write_reg(self.REG_DIR + self._port, new_dir_reg)
 
   def _get_slave(self):
     """Check and return needed params to call driver.
