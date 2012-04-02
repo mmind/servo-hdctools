d0034a8f6db62dc82de971ee531d06196d2fe174
Todd Broch <tbroch@chromium.org>
2012-04-02 14:16:13 -0700

    hdctools: servoflex_test_v2 script expanded to test legacy functionality.
    CL re-factors script to include testing of 40pin (legacy) connector
    and associated V1 test fixture.
    
    BUG=chrome-os-partner:8511
    TEST=manual,
         50 -> 42 pin servoflex V2 cables (connector:DUT_CONN_V2) via:
                 servoflex_test.py
         50 -> 50 pin servoflex V2 cables (connector:DUT_CONN_V2) via:
                 servoflex_test.py -p 50
         40 -> 40 pin servoflex V1 cables (connector:DUT_CONN_V1) via:
                 servoflex_test.py --legacy
    
    Change-Id: I85e2967d258a8432cf24b70c478da989d9d99619
    Reviewed-on: https://gerrit.chromium.org/gerrit/19702
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Commit-Ready: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

--- servo/scripts/servoflex_test_v2.py
+++ servo/scripts/servoflex_test_v2.py
@@ -2,7 +2,10 @@
 # Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Script to test servoflex_v2 50 pin -> 42 pin flexes."""
+"""Script to test servoflex cables attached to Servo V2 rev1|rev0.
+
+See usage ( -h ) for more details
+"""
 
 
 import logging
@@ -11,6 +14,7 @@ import os
 import select
 import subprocess
 import sys
+import string
 import time
 
 
@@ -37,6 +41,7 @@ def do_cmd(cmd, timeout, plist=None, flist=None):
         str: string, of stdout + stderr which is determined by presence of
         absence of plist & flist.
     """
+    retval = None
     logging.debug("cmd = %s", cmd)
     if type(cmd) is str:
         cmd = cmd.split()
@@ -58,16 +63,44 @@ def do_cmd(cmd, timeout, plist=None, flist=None):
                     for fail_str in flist:
                         if fail_str in log_str:
                             logging.error("FOUND: %s in %s", fail_str, log_str)
-                            return (False, cmd_obj, log_str)
+                            retval = False
+                            break
                 if plist:
                     for pass_str in plist:
                         if pass_str in log_str:
                             logging.debug("FOUND: %s in %s", pass_str,
                                          log_str)
-                            return (True, cmd_obj, log_str)
-    if flist:
-        return (True, cmd_obj, all_str)
-    return (None, cmd_obj, all_str)
+                            retval = True
+
+    if flist and retval is None:
+        retval = True
+
+    return (retval, cmd_obj, all_str)
+
+
+def launch_servod(options):
+    """Launches servod.
+
+    Args:
+      options: options from optparse
+
+    Returns:
+      retval: None|False|True, see do_cmd's retval.
+      pid: PID of servo process running.
+    """
+
+    cmd = 'sudo pkill servod'
+    subprocess.call(cmd, shell=True)
+    # launch servod
+    xml_files = '-c servoflex_test_v2.xml'
+    if options.legacy:
+        xml_files = '-c servoflex_test_v1.xml -c servoflex_v1.xml'
+    cmd = 'sudo servod -p 0x5002 %s' % xml_files
+    (retval, servod, _) = do_cmd(cmd, 5, plist=['Listening'],
+                                 flist=['Errno'])
+    logging.info("launch servod via %s", cmd)
+    time.sleep(3)
+    return (retval, servod)
 
 
 def set_ctrls(controls, timeout=0.2):
@@ -76,15 +109,15 @@ def set_ctrls(controls, timeout=0.2):
     Returns retval output from do_cmd
     """
     cmd = 'dut-control %s' % controls
-    (retval, _, _) = do_cmd(cmd, timeout, flist=['- ERROR -'])
+    (retval, _, _) = do_cmd(cmd, timeout, flist=['Errno', '- ERROR -'])
     return retval
 
 
-def get_ctrls(controls, timeout=0.2):
+def get_ctrls(controls, timeout=1):
     """Get various servod controls."""
     get_dict = {}
     cmd = 'dut-control %s' % controls
-    (retval, _, out) = do_cmd(cmd, timeout, flist=['- ERROR -'])
+    (retval, _, out) = do_cmd(cmd, timeout, flist=['Errno', '- ERROR -'])
     if retval:
         for ctrl_line in out.split('\n'):
             ctrl_line = ctrl_line.strip()
@@ -110,21 +143,29 @@ jtag_khz 1000
 jtag newtap auto0 tap -irlen 16 -expected-id 0xd5044093
 """
 
-
 # TODO(tbroch) Must sudo USE=ftdi emerge openocd
-def test_jtag():
+def test_jtag(options):
     """ Test JTAG interface.
+
+    Args:
+      options: options from optparse
 
     Returns True if passes, Fail otherwise
     """
-
     errors = 0
-    on_ctrls = 'spi2_vref:pp3300 jtag_buf_en:on jtag_buf_on_flex_en:on'
-    off_ctrls = 'spi2_vref:off jtag_buf_en:off jtag_buf_on_flex_en:off'
+    ctrls = ['jtag_buf_en:{val}']
+    if options.legacy:
+        ctrls.extend(['bios_en:{val}', 'jtag_vref_sel1:{pwr}',
+                      'jtag_vref_sel0:{pwr}'])
+        # warm_reset/pch_disable used to save a buffer on test pcb alternate
+        # between access to SPI vs JTAG
+        ctrls.extend(['warm_reset:off', 'pch_disable:on'])
+    else:
+        ctrls.extend(['spi2_vref:{pwr}', 'jtag_buf_on_flex_en:{val}'])
 
-    if not set_ctrls(on_ctrls):
+    if not set_ctrls(' '.join(ctrls).format(pwr='pp3300', val='on')):
         logging.error('Enabling access to JTAG')
-        set_ctrls(off_ctrls)
+        set_ctrls(' '.join(ctrls).format(pwr='off', val='off'))
         return False
 
     fname = '/tmp/servoflex_test_openocd.cfg'
@@ -143,7 +184,7 @@ def test_jtag():
     subprocess.call(cmd, shell=True)
     # TODO(tbroch) should we stress jtag here?  Currently only get TAP's IDCODE
 
-    if not set_ctrls(off_ctrls):
+    if not set_ctrls(' '.join(ctrls).format(pwr='off', val='off')):
         logging.error('Disabling access to JTAG')
         errors += 1
     return (errors == 0)
@@ -153,7 +194,7 @@ FLASHROM_PASS = ['probe_spi_rems: id1 0xbf, id2 0x48']
 FLASHROM_FAIL = None
 
 
-def test_spi(dev_id):
+def test_spi(dev_id, options):
     """ Test SPI interface.
 
     TODO(tbroch) actual part is SST25VF512A.  See about adding it
@@ -161,86 +202,107 @@ def test_spi(dev_id):
 
     Args:
       dev_id: integer, number corresponding to servod controls that operate this
-        SPI interface.  Should be 1 | 2
+        SPI interface.  Should be 0 | 1 | 2.
+      options: options from optparse
 
     Returns True if passes, Fail otherwise
     """
-    assert dev_id == 1 or dev_id == 2, 'SPI dev_id should be 1 | 2'
+    assert dev_id >= 0 and dev_id <= 2, 'SPI dev_id should be 0 | 1 | 2'
+    id_str = "%d" % dev_id
     errors = 0
-    ctrls = 'spi%d_vref:pp3300 spi%d_buf_en:on ' % (dev_id, dev_id)
-    ctrls += 'spi%d_buf_on_flex_en:on spi_hold:off' % (dev_id)
+    ctrls = []
+    cmd = 'sudo flashrom -V -p ft2232_spi:type=servo-v2'
+    if options.legacy:
+        cmd += '-legacy'
+        ctrls.extend(['jtag_vref_sel1:{pwr}', 'jtag_vref_sel0:{pwr}',
+                      'jtag_buf_en:{val}', 'bios_en:{val}'])
+        # warm_reset/pch_disable used to save a buffer on test pcb alternate
+        # between access to SPI vs JTAG
+        ctrls.extend(['warm_reset:on', 'pch_disable:off'])
+    else:
+        ctrls.extend(['spi{id}_vref:{pwr}', 'spi{id}_buf_en:{val}',
+                      'spi{id}_buf_on_flex_en:{val}', 'spi_hold:off'])
 
-    if not set_ctrls(ctrls):
-        logging.error('enabling access to spi %d', dev_id)
+
+    if not set_ctrls(' '.join(ctrls).format(id=id_str, pwr='pp3300',
+                                            val='on')):
+        logging.error('enabling access to spi %s', id_str)
         return False
 
-    cmd = 'sudo flashrom -V -p ft2232_spi:type=servo-v2'
     if dev_id == 1:
         cmd += ',port=b'
     cmd += ' -c SST25VF040'
     (retval, flash, _) = do_cmd(cmd, 5, plist=FLASHROM_PASS,
                             flist=FLASHROM_FAIL)
     if not retval:
-        logging.error('reading eeprom for spi %d', dev_id)
+        logging.error('reading eeprom for spi %s', id_str)
         errors += 1
 
-    ctrls = 'spi%d_vref:off spi%d_buf_en:off ' % (dev_id, dev_id)
-    ctrls += 'spi%d_buf_on_flex_en:off spi_hold:off' % (dev_id)
-
-    if not set_ctrls(ctrls):
-        logging.error("disabling access to spi %d", dev_id)
+    if not set_ctrls(' '.join(ctrls).format(id=id_str, pwr='off',
+                                            val='off')):
+        logging.error("disabling access to spi %s", id_str)
         errors += 1
 
     flash.terminate()
     return (errors == 0)
 
 
-def test_uart(dev_id):
+def test_uart(dev_id, options):
     """ Test UART interface.
 
     Args:
       dev_id: integer, number corresponding to servod controls that operate this
-        UART interface.  Should be 1 | 2
+        UART interface.  Should be 1 | 2 | 3
+      options: options from optparse
 
     Returns True if passes, Fail otherwise
     """
     errors = 0
-    assert dev_id == 1 or dev_id == 2, 'UART dev_id should be 1 | 2'
-    ctrls = "uart%d_en:on" % (dev_id)
-    if not set_ctrls(ctrls):
-        logging.error('Enabling access to UART %d', dev_id)
+    assert dev_id >= 1 and dev_id <= 3, 'UART dev_id should be 1 | 2 | 3'
+
+    id_str = '%d' % dev_id
+    ctrls = ['uart{id}_en:{val}']
+    if options.legacy:
+        ctrls.extend(['spi1_vref:{pwr}', 'rx_en:{val}', 'tx_en:{val}'])
+
+    if not set_ctrls(' '.join(ctrls).format(id=id_str, pwr='pp3300',
+                                            val='on')):
+        logging.error('Enabling access to UART %s', id_str)
         return False
 
-    ctrls = "uart%d_pty" % (dev_id)
-    (retval, get_dict) = get_ctrls(ctrls)
+    (retval, get_dict) = get_ctrls("uart%s_pty" % (id_str))
     if not retval:
-        logging.error('Retrieving pty to UART %d', dev_id)
+        logging.error('Retrieving pty to UART %s', id_str)
         errors += 1
 
     if not errors:
-        fd = os.open(get_dict['uart%d_pty' % dev_id], os.O_RDWR)
-        send_str = 'hello %d' % dev_id
+        fd = os.open(get_dict['uart%s_pty' % id_str], os.O_RDWR)
+        send_str = 'hello %s' % id_str
         os.write(fd, send_str)
         (rfds, _, _) = select.select([fd], [], [], 1)
-        if len(rfds) > 0:
-            rsp_str = os.read(fd, len(send_str))
-            if rsp_str != send_str:
-                logging.error('Sent != received for UART %d', dev_id)
-                errors += 1
-        else:
-            logging.error('Nothing received for UART %d', dev_id)
+        rsp_str = ''
+        reread_count = 0
+        while len(rfds) > 0 and reread_count < 1000:
+            rsp_str += os.read(fd, len(send_str))
+            (rfds, _, _) = select.select([fd], [], [], 1)
+            reread_count += 1
+
+        rsp_str = ''.join(filter(lambda x: x in string.printable, rsp_str))
+        if rsp_str != send_str:
+            logging.error('Sent(%s) != Rcv(%s) for UART %s', send_str,
+                           rsp_str, id_str)
             errors += 1
         os.close(fd)
 
-    ctrls = "uart%d_en:off" % (dev_id)
-    if not set_ctrls(ctrls):
-        logging.error("Disabling access to UART %d", dev_id)
+    if not set_ctrls(' '.join(ctrls).format(id=id_str, pwr='off', val='off')):
+        logging.error("Disabling access to UART %s", id_str)
         errors += 1
     return (errors == 0)
 
 
 GPIO_MAPS = {'off': 'on', 'on': 'off', 'press': 'release',
-             'release': 'press', 'yes': 'no', 'no': 'yes'}
+             'release': 'press', 'yes': 'no', 'no': 'yes',
+             'ERR': 'error on initial read'}
 
 KBD_MUX_COL_IDX = ['kbd_m2_a', 'kbd_m1_a']
 
@@ -278,7 +340,7 @@ def test_kbd_gpios():
                 errors += 1
             for set_val in [GPIO_MAPS[ctrls[kbd_col]], ctrls[kbd_col]]:
                 cmd = '%s:%s sleep:0.2 %s' % (kbd_row, set_val, kbd_col)
-                (retval, ctrls) = get_ctrls(cmd, timeout=1)
+                (retval, ctrls) = get_ctrls(cmd)
                 if not retval:
                     logging.error('ctrls = %s', ctrls)
                     errors += 1
@@ -290,7 +352,7 @@ def test_kbd_gpios():
     return errors
 
 
-def test_gpios(pins):
+def test_gpios(options):
     """Test GPIO's across the servoflex connector.
 
     The GPIO's are routed from the servo through the servoflex to the test
@@ -306,23 +368,23 @@ def test_gpios(pins):
       emulation of OD style GPIO's.
 
     Args:
-      pins:  Number of pins flex has on DUT side
+      options: options from optparse
 
     Returns, True if passes, Fail otherwise
     """
+    pins = options.pins
     assert (pins == 50) or (pins == 42), 'Pins must be 42 | 50'
     gpio_prefix = ['test_']
     if (pins == 50):
       gpio_prefix.append('test50_')
 
     errors = 0
-    cmd = 'i2c_mux_en:on i2c_mux:rem '
-    cmd += 'sd_vref_sel:pp3300 sd_en:on '
-    cmd += 'spi2_vref:pp3300 fw_wp_en:on '
-    if not set_ctrls(cmd):
+    cmd = ['spi2_vref:{pwr}', 'sd_vref_sel:{pwr}', 'sd_en:{val}',
+           'fw_wp_en:{val}', 'jtag_vref_sel1:{pwr}', 'jtag_vref_sel0:{pwr}']
+    if not set_ctrls(' '.join(cmd).format(pwr="pp3300", val="on")):
         logging.error('Steering i2c mux to remote')
         return False
-    (retval, all_ctrls) = get_ctrls('', timeout=1)
+    (retval, all_ctrls) = get_ctrls('', timeout=2)
     if not retval:
         logging.error('Getting all ctrls')
         return False
@@ -336,7 +398,7 @@ def test_gpios(pins):
     for _ in xrange(2):
         for set_name, get_name in gpios_to_test.iteritems():
             set_val = GPIO_MAPS[all_ctrls[set_name]]
-            logging.debug("%s %s -> %s", set_name,
+            logging.debug("Trying %s %s -> %s", set_name,
                           all_ctrls[set_name], set_val)
             (retval, ctrl) = get_ctrls('%s:%s %s' % (set_name, set_val,
                                                      get_name))
@@ -349,18 +411,15 @@ def test_gpios(pins):
                                   all_ctrls[set_name], set_val)
                     errors += 1
                 else:
-                    logging.info('GPIO %s from %s -> %s', set_name,
-                                 all_ctrls[set_name], set_val)
+                    logging.debug('Done GPIO %s from %s -> %s', set_name,
+                                  all_ctrls[set_name], set_val)
 
             all_ctrls[set_name] = set_val
 
-    if pins == 50:
+    if pins == 50 or options.legacy:
         errors += test_kbd_gpios()
 
-    cmd = 'i2c_mux_en:off i2c_mux:rem '
-    cmd += 'sd_vref_sel:off sd_en:off '
-    cmd += 'spi2_vref:off fw_wp_en:off'
-    if not set_ctrls(cmd):
+    if not set_ctrls(' '.join(cmd).format(pwr="pp3300", val="on")):
         logging.error('Disabling i2c mux to remote')
         errors += 1
 
@@ -373,16 +432,30 @@ VERSION = "0.0.1"
 
 def parse_args():
     description = ('')
-    examples = ('')
+    examples = (
+        '\nScript to test servoflex cables attached to Servo V2 rev1|rev0.\n'
+        '\n\tCan be used to test:\n'
+        '\t\t50 -> 42 pin servoflex V2 cables (connector:DUT_CONN_V2) via:\n'
+        '\t\t\tservoflex_test.py\n'
+        '\t\t50 -> 50 pin servoflex V2 cables (connector:DUT_CONN_V2) via:\n'
+        '\t\t\tservoflex_test.py -p 50\n'
+        '\t\t40 -> 40 pin servoflex V1 cables (connector:DUT_CONN_V1) via:\n'
+        '\t\t\tservoflex_test.py --legacy\n'
+        )
     parser = optparse.OptionParser(version="%prog "+VERSION)
     parser.description = description
     parser.add_option("-d", "--debug", action="store_true", default=False,
                       help="enable debug messages")
     parser.add_option("-p", "--pins", type=int, default=42,
                       help="Pin width of flex on DUT side.  Either 42 | 50")
+    parser.add_option("-l", "--legacy", action="store_true", default=False,
+                      help="Test legacy 40pin connector")
     parser.set_usage(parser.get_usage() + examples)
     return parser.parse_args()
 
+
+V2_TESTS = ['jtag(', 'uart(1,', 'uart(2,', 'spi(1,', 'spi(2,', 'gpios(']
+LEGACY_TESTS = ['jtag(', 'uart(3,', 'spi(0,', 'gpios(']
 
 def main():
     errors = 0
@@ -395,23 +468,28 @@ def main():
 
     log_format += " - %(message)s"
     logging.basicConfig(level=loglevel, format=log_format)
-
-    cmd = 'sudo pkill servod'
-    subprocess.call(cmd, shell=True)
-    # launch servod
-    cmd = 'sudo servod -c servoflex_test_v2.xml'
-    (retval, servod, _) = do_cmd(cmd, 3, plist=['Listening'])
-    if retval:
+    (success, servod) = launch_servod(options)
+    if success:
         logging.info('Started servod')
-        for test in ['gpios(%d)' % options.pins, 'jtag()', 'uart(1)', 'uart(2)',
-                     'spi(1)', 'spi(2)']:
-            logging.info("Start  Test -> %s", test)
 
-            retval = eval('test_%s' % test)
+        # steer i2c mux to remote for any IC's on flex or test fixture
+        if not set_ctrls('i2c_mux_en:on i2c_mux:rem'):
+            logging.error('Enabling i2c mux to remote')
+            return -1
+
+        tests = V2_TESTS
+        if options.legacy:
+            tests = LEGACY_TESTS
+
+        for test in tests:
+            test_fn = 'test_%soptions)' % test
+            logging.info("<------  START :: %s ------>", test_fn)
+
+            retval = eval(test_fn)
             if not retval:
-                logging.error('%s FAILED', test)
+                logging.error('%s FAILED', test_fn)
                 errors += 1
-            logging.info("Finish Test -> %s", test)
+            logging.info("<------ FINISH :: %s ------>", test_fn)
     else:
         logging.error('Servod launch failed')
         errors += 1
