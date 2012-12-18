f161fbffe763c9ee9a3deebbf0fd522e18532a97
Todd Broch <tbroch@chromium.org>
2012-12-18 13:04:43 -0800

    hdctools: servod: spring ADCs for power measurement.
    BUG=none
    TEST=manual,
    1. Emerge it
    sudo emerge hdctools
    
    2. launch servod
    sudo servod -c spring.xml
    
    3.  Take measurments
    spring_mv=`dut-control -i | grep _mv | cut -d' ' -f1`
    spring_ma=`dut-control -i | grep _ma | cut -d' ' -f1`
    spring_mw=`dut-control -i | grep _mw | cut -d' ' -f1`
    dut-control $spring_mv
    dut-control $spring_ma
    dut-control $spring_mw
    
    Values read make sense.
    
    Change-Id: I0064c689f7a40cf39ea727c91a0dbfa50211c6bc
    Reviewed-on: https://gerrit.chromium.org/gerrit/39881
    Reviewed-by: Aaron Eash <ame@google.com>
    Reviewed-by: Mark Hayter <mdhayter@chromium.org>
    Commit-Ready: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

---
+++ servo/data/spring.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [(0x40, 'ap_mif', 1, 0.01, 'rem', True),
+        (0x41, 'pp3300', 3.3, 0.01, 'rem', True),
+        (0x42, 'ap_arm', 1, 0.01, 'rem', True),
+        (0x43, 'pp5000', 5, 0.01, 'rem', True),
+        (0x44, 'ap_g3d', 1, 0.01, 'rem', True),
+        (0x48, 'pp1350_ddr', 1.35, 0.01, 'rem', True),
+        (0x4b, 'pp1800', 1.8, 0.01, 'rem', True),
+        (0x4c, 'ap_int', 1, 0.01, 'rem', True)
+        ]
