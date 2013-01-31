bb954d3423be9a55ddbc17bced4d902d2ff68ada
Todd Broch <tbroch@chromium.org>
2013-01-30 16:45:20 -0800

    hdctools: servod: Link sense resistor changes.
    BUG=none
    TEST=none
    Change-Id: I0c10cf0aaa0e2dfe0186c91464bec402975cc17f
    Reviewed-on: https://gerrit.chromium.org/gerrit/42356
    Tested-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- servo/data/link.py
+++ servo/data/link.py
@@ -1,13 +1,12 @@
-# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 inas = [
-    (0x41, 'pp5000_alw', 5, 0.01, 'rem', True),
-    (0x42, 'pp3300_alw', 3.3, 0.01, 'rem', True),
-    (0x43, 'pp1050', 1.05, 0.01, 'rem', True),
-    (0x44, 'pp1800', 1.8, 0.01, 'rem', True),
-    (0x48, 'pp1350_ddr', 1.35, 0.01, 'rem', True),
-    (0x49, 'ppvar_bl', 48, 0.01, 'rem', True),
-    (0x4B, 'pp3300_wwan', 3.3, 0.01, 'rem', True),
-    (0x4C, 'pp3300_wlan', 3.3, 0.01, 'rem', True)
+    (0x41, 'pp5000_alw', 5, 0.005, 'rem', True),
+    (0x42, 'pp3300_alw', 3.3, 0.005, 'rem', True),
+    (0x44, 'pp1800', 1.8, 0.005, 'rem', True),
+    (0x48, 'pp1350_ddr', 1.35, 0.005, 'rem', True),
+    (0x49, 'ppvar_bl', 48, 0.005, 'rem', True),
+    (0x4B, 'pp3300_wwan', 3.3, 0.005, 'rem', True),
+    (0x4C, 'pp3300_wlan', 3.3, 0.005, 'rem', True)
     ]
