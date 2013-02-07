e63403240810c5e7db7740f2abcfbeac4c9454f6
Todd Broch <tbroch@chromium.org>
2013-02-06 18:23:47 -0800

    hdctools: servod: spring ADCs for power measurement.
    Note this is part two.  Original ADC/sense moved to spring_v0_r0.py.
    Those boards were mostly deprecated.  If you need to access those
    ADCs consult use that file instead.
    
    BUG=chrome-os-partner:17608
    TEST=manual,
    1. Emerge it
    sudo emerge hdctools
    
    2. launch servod
    sudo servod -c spring.xml
    
    3.  Take measurments
    spring_mv=`dut-control -i | grep _mv | cut -d' ' -f1`
    spring_ma=`dut-control -i | grep _ma | cut -d' ' -f1`
    spring_mw=`dut-control -i | grep _mw | cut -d' ' -f1`
    dut-control $spring_mv
    dut-control $spring_ma
    dut-control $spring_mw
    
    Values read make sense.
    
    Change-Id: I4f5939d13b0cf3efceda866db296a6086dc5ccf9
    Reviewed-on: https://gerrit.chromium.org/gerrit/42841
    Reviewed-by: Bryan Freed <bfreed@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

--- servo/data/spring.py
+++ servo/data/spring.py
@@ -1,12 +1,20 @@
-# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-inas = [(0x40, 'ap_mif', 1, 0.01, 'rem', True),
-        (0x41, 'pp3300', 3.3, 0.01, 'rem', True),
-        (0x42, 'ap_arm', 1, 0.01, 'rem', True),
-        (0x43, 'pp5000', 5, 0.01, 'rem', True),
-        (0x44, 'ap_g3d', 1, 0.01, 'rem', True),
-        (0x48, 'pp1350_ddr', 1.35, 0.01, 'rem', True),
-        (0x4b, 'pp1800', 1.8, 0.01, 'rem', True),
-        (0x4c, 'ap_int', 1, 0.01, 'rem', True)
-        ]
+inas = [(0x40, 'pp1800', 1.8, 0.01, 'loc0', True),
+        (0x41, 'ap_mif', 1, 0.01, 'loc0', True),
+        (0x42, 'ap_g3d', 1, 0.005, 'loc0', True),
+        (0x43, 'ap_arm', 1, 0.005, 'loc0', True),
+        (0x44, 'ap_int', 1, 0.01, 'loc0', True),
+        (0x45, 'pp2000_ldoin', 2, 0.01, 'loc0', True),
+        (0x46, 'pp1350_ldoin', 1.35, 0.01, 'loc0', True),
+        (0x47, 'pp3300_ec', 3.3, 2.2, 'loc0', True),
+        (0x48, 'pp1200_edp', 1.2, 0.6, 'loc0', True),
+        (0x49, 'pp1800_mydp', 1.8, 0.8, 'loc0', True),
+        (0x4A, 'pp1000_mydp', 1, 0.8, 'loc0', True),
+        (0x4B, 'pp1350_ddr', 1.35, 0.01, 'loc0', True),
+        (0x4C, 'pp3300', 3.3, 0.01, 'loc0', True),
+        (0x4D, 'pp5000', 5, 0.01, 'loc0', True),
+        (0x4E, 'pp3300_alw', 3.3, 0.2, 'loc0', True),
+        (0x4F, 'ppvar_vbat', 14, 0.01, 'loc0', True)]
+
