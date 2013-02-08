8c1022bb7274e6232aefe654ac379af0744caff2
Todd Broch <tbroch@chromium.org>
2013-02-08 15:30:04 -0800

    hdctools: servod: mcCroskey ADCs for power measurement.
    BUG=none
    TEST=manual,
    1. Emerge it
    sudo emerge hdctools
    
    2. launch servod
    sudo servod -c mccroskey.xml
    
    3.  Take measurments
    mv=`dut-control -i | grep _mv | cut -d' ' -f1`
    ma=`dut-control -i | grep _ma | cut -d' ' -f1`
    mw=`dut-control -i | grep _mw | cut -d' ' -f1`
    dut-control $mv
    dut-control $ma
    dut-control $mw
    
    Values read make sense.
    
    Change-Id: I32ba3dfa2a94483b6d85d0338c39bf7962418d8a
    Reviewed-on: https://gerrit.chromium.org/gerrit/42965
    Reviewed-by: Neil Hendin <nhendin@chromium.org>
    Tested-by: Neil Hendin <nhendin@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

---
+++ servo/data/mccroskey.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [(0x40, 'usb_vbus_up', 5, 0.1, 'rem', True),
+        (0x41, 'usb_vbus_dn', 5, 0.1, 'rem', True),
+        (0x42, 'pp3300_ec', 3.3, 0.39, 'rem', True),
+        (0x43, 'pp3300_hub', 3.3, 0.39, 'rem', True),
+        (0x44, 'pp3300_widi', 3.3, 0.39, 'rem', True),
+        ]
