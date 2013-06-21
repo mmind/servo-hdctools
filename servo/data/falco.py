09a814b0332d7afe8a94721bdfc40bd29465b6a1
Sameer Nanda <snanda@chromium.org>
2013-06-21 16:40:27 -0700

    servod: falco: Add ADCs for power measurement.
    BUG=none
    TEST=manual
    1. Emerge it
    sudo emerge hdctools
    
    2. launch servod on servo V1 & swing the mux to the adcs
    sudo servod -c falco.xml
    dut-control i2c_mux:loc0
    
    3.  Take measurments
    falco_mv=`dut-control -i | grep _mv | cut -d' ' -f1`
    falco_ma=`dut-control -i | grep _ma | cut -d' ' -f1`
    falco_mw=`dut-control -i | grep _mw | cut -d' ' -f1`
    dut-control $falco_mv
    dut-control $falco_ma
    dut-control $falco_mw
    
    Values read make sense.
    
    Change-Id: I3b264ac4dacc1d627824716a1308f19c8b051948
    Reviewed-on: https://gerrit.chromium.org/gerrit/59647
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Tested-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Sameer Nanda <snanda@chromium.org>

---
+++ servo/data/falco.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [
+    (0x40, 'pp5000', 5.0, 0.01, 'loc0', True),            # PL8
+    (0x41, 'pp3300_dsw', 3.3, 0.01, 'loc0', True),        # PL9
+    (0x42, 'pp1350', 1.35, 0.01, 'loc0', True),           # PL11
+    (0x43, 'pp1050_pch_sus', 1.05, 0.01, 'loc0', True),   # PL13
+    (0x44, 'pp1500', 1.5, 0.1, 'loc0', True),             # PL16
+    (0x45, 'vin', 19, 0.01, 'loc0', True),                # PR223
+    ]
