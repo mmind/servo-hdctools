00ea0118c8afaeae5f5d15dbfef2243937a11638
Todd Broch <tbroch@chromium.org>
2013-10-29 16:21:27 -0700

    Add rambi power measurements
    BUG=chrome-os-partner:23725
    TEST=manual, measure power ... readings make sense
    
    Change-Id: I86c7b6e8cf1251eb6bf335f9dd0b45fd165546b1
    Reviewed-on: https://chromium-review.googlesource.com/175417
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

---
+++ servo/data/rambi.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [(0x41, 'ppvin_pp5000', 7.5, 0.03, 'rem', True),
+        (0x42, 'ppvin_pp3300', 7.5, 0.02, 'rem', True),
+        (0x49, 'ppvin_pp1350', 7.5, 0.1, 'rem', True),
+        (0x43, 'pp3300_pp1050', 3.3, 0.1, 'rem', True),
+        (0x47, 'pp3300_pp1000', 3.3, 0.1, 'rem', True),
+        ]
