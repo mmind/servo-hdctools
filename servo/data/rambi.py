fd55433a30bd386c236c9b5bbc9dce510dc5863e
Todd Broch <tbroch@chromium.org>
2013-12-03 09:17:43 -0800

    rambi: renames of voltage rails for 1v5.
    BUG=none
    TEST=manual
    
    Change-Id: Ibdf414790faaed0151d4f6f1250e52a4af87dbe3
    Reviewed-on: https://chromium-review.googlesource.com/178578
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

--- servo/data/rambi.py
+++ servo/data/rambi.py
@@ -4,6 +4,6 @@
 inas = [(0x41, 'ppvin_pp5000', 7.5, 0.03, 'rem', True),
         (0x42, 'ppvin_pp3300', 7.5, 0.02, 'rem', True),
         (0x49, 'ppvin_pp1350', 7.5, 0.1, 'rem', True),
-        (0x43, 'pp3300_pp1050', 3.3, 0.1, 'rem', True),
-        (0x47, 'pp3300_pp1000', 3.3, 0.1, 'rem', True),
+        (0x43, 'ppvin_pp1050', 7.5, 0.1, 'rem', True),
+        (0x47, 'ppvin_pp1000', 7.5, 0.1, 'rem', True),
         ]
