807a08a5ea062a9011bdec09ac7b2dbadfdf6c94
Bryan Freed <bfreed@chromium.org>
2013-12-09 15:21:00 -0800

    Nyan: Update sense resistors for norrin rails.
    This is a 3s battery system now, so some of the rails are renamed.
    
    BUG=chrome-os-partner:23860
    TEST='servod -b nyan -c nyan.xml' works for power measurements.
    
    Change-Id: Idb5c070e7325c2c3c01b061a54b3d3466cc4b018
    Reviewed-on: https://chromium-review.googlesource.com/179856
    Tested-by: Bryan Freed <bfreed@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Bryan Freed <bfreed@chromium.org>

--- servo/data/nyan.py
+++ servo/data/nyan.py
@@ -24,18 +24,39 @@
 #  5v_stby (not listed here)
 #   3.3v_stby
 
-inas = [(0x40, 'vdd_mux', 7.4, 0.02, 'rem', True),
-        (0x41, '5v_sys', 5, 0.01, 'rem', True),
+# Norrin (pm370)
+# vdd_mux
+#  vdd_lcd_bl
+#  5v_sys
+#   1.8v_vddio
+#    1.8v_vdd_wf
+#   1.35v_lp0 (not listed here)
+#    vddio_ddr_ap
+#    vddio_dram
+#   5v_core
+#   5v_cpu
+#   5v_gpu
+#  3.3v_sys
+#   3.3v_run (not listed here)
+#    3.3v_panel
+#    3.3v_usbcam (not listed here)
+#   3.3v_lp0 (not listed here)
+#    3.3v_vdd_wf
+#  5v_stby (not listed here)
+#   3.3v_stby
+
+inas = [(0x40, 'vdd_mux', 11.1, 0.01, 'rem', True),
+        (0x41, '5v_sys', 5, 0.003, 'rem', True),
         (0x42, '3_3v_sys', 3.3, 0.01, 'rem', True),
         (0x44, '1_8v_vddio', 1.8, 0.01, 'rem', True),
-        (0x45, 'vdd_mux_core', 7.4, 0.01, 'rem', True),
-        (0x46, 'vdd_mux_cpu', 7.4, 0.01, 'rem', True),
+        (0x45, '5v_core', 5, 0.01, 'rem', True),
+        (0x46, '5v_cpu', 5, 0.01, 'rem', True),
         (0x48, 'vddio_ddr_ap', 1.35, 0.01, 'rem', True),
         (0x49, 'vddio_dram', 1.35, 0.01, 'rem', True),
-        (0x4A, 'vdd_lcd_bl', 7.4, 0.01, 'rem', True),
-        (0x4B, '3_3v_panel', 3.3, 0.01, 'rem', True),
-        (0x4C, 'vdd_mux_gpu', 7.4, 0.01, 'rem', True),
-        (0x4D, '1_8v_vdd_wf', 1.8, 0.2, 'rem', True),
+        (0x4A, 'vdd_lcd_bl', 11.1, 0.01, 'rem', True),
+        (0x4B, '3_3v_panel', 3.3, 0.02, 'rem', True),
+        (0x4C, '5v_gpu', 5, 0.01, 'rem', True),
+        (0x4D, '1_8v_vdd_wf', 1.8, 0.5, 'rem', True),
         (0x4E, '3_3v_vdd_wf', 3.3, 0.01, 'rem', True),
         (0x4F, '3_3v_stby', 3.3, 0.2, 'rem', True)]
 
