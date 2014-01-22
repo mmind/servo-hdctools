17a77c717c9e9ae0351d9923e0e9e46089b4c6fe
Derek Basehore <dbasehore@chromium.org>
2014-01-21 17:21:52 -0800

    hdctools: servod: Update rambi adcs for power measurement
    This updates the adcs for the new board.
    
    BUG=chrome-os-partner:23628
    TEST=dut-control
    	Check that all of the ppvin_pp* values are valid
    
    Change-Id: If0ae55b333e0ad18aaa40c6b69fda606946f8390
    Reviewed-on: https://chromium-review.googlesource.com/183366
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Tested-by: Derek Basehore <dbasehore@chromium.org>
    Commit-Queue: Derek Basehore <dbasehore@chromium.org>

---
+++ servo/data/rambi_2v0_r0_loc.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [(0x40, 'ppvin_pp5000', 7.5, 0.03, 'loc0', True),
+        (0x41, 'ppvin_pp3300', 7.5, 0.02, 'loc0', True),
+        (0x42, 'ppvin_pp1350', 7.5, 0.1, 'loc0', True),
+        (0x43, 'ppvin_pp1050', 7.5, 0.1, 'loc0', True),
+        (0x44, 'ppvin_pp1000', 7.5, 0.1, 'loc0', True),
+        (0x45, 'ppvin_gfx', 7.5, 0.03, 'loc0', True),
+        (0x46, 'ppvin_core', 7.5, 0.03, 'loc0', True),
+        ]
