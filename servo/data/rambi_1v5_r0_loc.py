94240b1463541ed10fd7c1dfd0b1517c9ba400a5
Todd Broch <tbroch@chromium.org>
2014-01-22 14:04:49 -0800

    Add rambi power measurements ADCs.
    These are sense resistors wired over to servo V1 local ADCs on 1v5.
    
    BUG=chrome-os-partner:23725
    TEST=manual, measure power ... readings make sense
    
    Change-Id: Ica12e149f7d1db95723f60a84bd4d633669f598d
    Reviewed-on: https://chromium-review.googlesource.com/183524
    Tested-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Derek Basehore <dbasehore@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

---
+++ servo/data/rambi_1v5_r0_loc.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [(0x40, 'ppvin', 7.5, 0.03, 'loc0', True),
+        (0x41, 'pp3300_ec', 3.3, 2.2, 'loc0', True),
+        (0x42, 'pp1800_pch', 1.8, 1.0, 'loc0', True),
+        (0x43, 'ppvin_gfx', 1.0, 0.03, 'loc0', True),
+        (0x44, 'ppvin_cpu', 1.0, 0.03, 'loc0', True),
+        (0x45, 'pp1000_pch_s5', 1.0, 0.051, 'loc0', True),
+        (0x46, 'pp1350_pch', 1.35, 0.2, 'loc0', True),
+        (0x47, 'pp1800_pch_s5', 1.8, 1.0, 'loc0', True),
+        (0x48, 'pp3300_pch', 3.3, 1.0, 'loc0', True),
+        (0x49, 'ppcpu', 1.1, 0.01, 'loc0', True),
+        (0x4a, 'pp3300_wlan', 3.3, 0.2, 'loc0', True),
+        (0x4c, 'ppgfx', 1.1, 0.01, 'loc0', True),
+        ]
