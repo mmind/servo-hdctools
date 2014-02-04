5c66e551be78caccc90f86772b81e3a3a4343d62
Vadim Bendebury <vbendeb@chromium.org>
2014-02-04 11:28:08 -0800

    Add pi confguration files/settings
    Create necessary configuration items to be able to run servod for the
    pi board.
    
    BUG=chrome-os-partner:25477
    TEST=manual
       . the following commands succeed:
       $ sudo emerge hdctools
       $ sudo servod -s 686203-00062 --board=peach_pi
    
    Change-Id: I14673624a73d5b800b030becc2d4de86504a2f47
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/184906
    Reviewed-by: Todd Broch <tbroch@chromium.org>

---
+++ servo/data/pi.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+inas = [(0x40, 'ap_arm', 1, 0.01, 'loc0', True),
+        (0x41, 'ap_g3d', 1, 0.01, 'loc0', True),
+        (0x42, 'ap_int', 1, 0.1, 'loc0', True),
+        (0x43, '1350_ldoin', 1.35, 0.1, 'loc0', True),
+        (0x44, 'ap_mif', 1, 0.1, 'loc0', True),
+        (0x45, 'ap_kfc', 1, 0.1, 'loc0', True),
+        (0x46, '2000_ldoin', 2, 0.1, 'loc0', True)
+        ]
