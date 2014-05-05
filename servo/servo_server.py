f57b7bd7fe6f555d680c01a797b25458f009e742
Mohammed Habibulla <moch@chromium.org>
2014-05-05 13:49:49 -0700

    Tricky: Add servo overlay for tricky
    BUG=none
    TEST=sudo servod --board=tricky
    
    Change-Id: I9ca410cc9d6c0c38bc723e5c539af73a408c6f40
    Reviewed-on: https://chromium-review.googlesource.com/198226
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>
    Commit-Queue: Mohammed Habibulla <moch@chromium.org>
    Tested-by: Mohammed Habibulla <moch@chromium.org>

--- servo/servo_server.py
+++ servo/servo_server.py
@@ -127,7 +127,7 @@ class Servod(object):
       return keyboard_handlers.ParrotHandler(servo)
     elif board == 'stout':
       return keyboard_handlers.StoutHandler(servo)
-    elif board in ('monroe', 'panther', 'zako'):
+    elif board in ('monroe', 'panther', 'tricky', 'zako'):
       if self._usbkm232 is None:
         raise Exception("No device specified when "
                         "initializing usbkm232 keyboard handler")
