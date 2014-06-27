9e9e763d922810cc444a98027396cb16c676d76f
Vic Yang <victoryang@chromium.org>
2014-06-27 14:28:54 -0700

    Remove race condition in cros_ec_softrec_power
    In the current implementation, we must race to reboot the EC before the
    AP sets EC_IN_RW signal. To avoid this, let's hold warm_reset until the
    EC keeps the AP off.
    
    BUG=chrome-os-partner:29200
    TEST=pass firmware_DevMode on Squawks
    
    Change-Id: I07c21c057fc7c90cd28c8e37d0a58abcaf98f6ae
    Signed-off-by: Vic Yang <victoryang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/205981
    Reviewed-by: Yusuf Mohsinally <mohsinally@chromium.org>

--- servo/drv/cros_ec_softrec_power.py
+++ servo/drv/cros_ec_softrec_power.py
@@ -16,20 +16,26 @@ class crosEcSoftrecPower(cros_ec_power.CrosECPower):
 
   _HOSTEVENT_RECOVERY_CMD = 'hostevent set 0x4000'
 
-  # Time in seconds to allow the BIOS and EC to detect the
-  # 'rec_mode' signal after cold reset.
-  _RECOVERY_DETECTION_DELAY = 0.4
+  # Time in seconds to allow the EC to pick up the recovery
+  # host event.
+  _RECOVERY_DETECTION_DELAY = 1
 
   def _power_on(self, rec_mode):
     if rec_mode == self.REC_ON:
+      # Hold warm reset so the AP doesn't boot when EC reboots.
+      self._interface.set('warm_reset', 'on')
       # Reset the EC to force it back into RO code; this clears
       # the EC_IN_RW signal, so the system CPU will trust the
       # upcoming recovery mode request.
       self._cold_reset()
       # Restart the EC, but leave the system CPU off...
       self._interface.set('ec_uart_cmd', 'reboot ap-off')
-      time.sleep(self._RECOVERY_DETECTION_DELAY)
+      time.sleep(self._reset_recovery_time)
+      # Now the EC keeps the AP off. Release warm reset before powering
+      # on the AP.
+      self._interface.set('warm_reset', 'off')
       # ... and tell the EC to tell the CPU we're in recovery mode.
       self._interface.set('ec_uart_cmd',
                           self._HOSTEVENT_RECOVERY_CMD)
+      time.sleep(self._RECOVERY_DETECTION_DELAY)
     self._interface.power_short_press()
