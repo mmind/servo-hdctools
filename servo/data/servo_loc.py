4e881c106b49a7904099f1e155014156b0d58019
Todd Broch <tbroch@chromium.org>
2014-08-28 16:50:26 -0700

    Add adc_mux controls for INA adapter board.
    Note, PCA9540 is a 2to1 mux versus the PC9546 (4to1).
    
    BUG=chrome-os-partner:29886
    TEST=manual,
    
      sudo servod -c servo_loc.xml
    
      dut-control adc_mux:bank0
      ina_cfg=`dc -i | grep _cfg_reg | cut -d ' ' -f 1`
      dut-control $ina_cfg
      <See 0x399f for all cfgs>
      for c in $ina_cfg ; do $c:0x21ff $c ; done
      <See 0x21ff for all cfgs>
      dut-control adc_mux:bank1
      dut-control $ina_cfg
      <See 0x399f for all cfgs>
      for c in $ina_cfg ; do $c:0x21ff $c ; done
      <See 0x21ff for all cfgs>
    
    Change-Id: Ia31e95832d7cf540be0b5a395b2854551861e03d
    Reviewed-on: https://chromium-review.googlesource.com/215135
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- servo/data/servo_loc.py
+++ servo/data/servo_loc.py
@@ -1,6 +1,22 @@
 # Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+inline = """
+  <map>
+    <name>adc_mux</name>
+    <doc>valid mux values for DUT's two banks of INA219 off PCA9540
+    ADCs</doc>
+    <params none="0" bank0="4" bank1="5"></params>
+  </map>
+  <control>
+    <name>adc_mux</name>
+    <doc>4 to 1 mux to steer remote i2c i2c_mux:rem to two sets of
+    16 INA219 ADCs. Note they are only on leg0 and leg1</doc>
+    <params interface="2" drv="pca9546" slv="0x70"
+    map="adc_mux"></params>
+  </control>
+"""
+
 inas = [(0x40, 'loc_0x40', 19.5, 0.030, "loc0", False),
         (0x41, 'loc_0x41', 5.0,  0.010, "loc0", False),
         (0x42, 'loc_0x42', 3.3,  0.010, "loc0", False),
