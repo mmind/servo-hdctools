1166b3cf88cafd0735bf502092be90927109a120
Kevin K Wong <kevin.k.wong@intel.com>
2015-01-05 16:42:53 -0800

    strago: Added servo and INA support for Strago.
    BUG=none
    TEST=Verified servod can start with Strago as board.
    BRANCH=none
    
    Change-Id: I47ec6546d914448e05aad705b7be62ee2072614d
    Signed-off-by: Kevin K Wong <kevin.k.wong@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/238560
    Reviewed-by: Roy Mingi Park <roy.mingi.park@intel.com>
    Commit-Queue: Roy Mingi Park <roy.mingi.park@intel.com>
    Reviewed-by: Todd Broch <tbroch@chromium.org>

---
+++ servo/data/strago_proto.py
@@ -0,0 +1,17 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [(0x40, 'ina_v5a',         5.0,  0.002, 'rem', True),
+        (0x41, 'ina_v3p3a',       3.3,  0.002, 'rem', True),
+        (0x42, 'ina_vcc0_cpu',    7.5,  0.002, 'rem', True),
+        (0x43, 'ina_vddq',        7.5,  0.002, 'rem', True),
+        (0x44, 'ina_vbata',       7.5,  0.002, 'rem', True),
+        (0x45, 'ina_vgg',         7.5,  0.002, 'rem', True),
+        (0x46, 'ina_vnn',         7.5,  0.002, 'rem', True),
+        (0x47, 'ina_v1p05a',      1.05, 0.002, 'rem', True),
+        (0x48, 'ina_vddq_vtt',    7.5,  0.01,  'rem', True),
+        (0x49, 'ina_v1p15a',      1.15, 0.002, 'rem', True),
+        (0x4a, 'ina_v1p24a',      1.24, 0.002, 'rem', True),
+        (0x4b, 'ina_v1p8a',       1.8,  0.002, 'rem', True),
+        (0x4c, 'ina_v3p3a_prime', 3.3,  0.002, 'rem', True),
+        ]
