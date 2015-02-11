2b8466b8f42caa034d08afe40252ff1bae3c5516
Chung-yih Wang <cywang@chromium.org>
2015-02-11 17:15:58 +0800

    oak: Add servo overlay
    BUG=chrome-os-partner:36631
    TEST=emerge hdctools;
      sudo servod -c oak.xml &
      mv=`dut-control -i|grep _mv | cut -d ' ' -f1`
      dut-control $mv
    
    Change-Id: Id5c3054cc9667c1e48498aea890c69e84e66ecd8
    Reviewed-on: https://chromium-review.googlesource.com/248522
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>
    Trybot-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Chung-yih Wang <cywang@chromium.org>
    Trybot-Ready: Chung-yih Wang <cywang@chromium.org>
    Tested-by: Chung-yih Wang <cywang@chromium.org>

---
+++ servo/data/oak.py
@@ -0,0 +1,41 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# MTK OAK board sense resistors feed INA219s on the board.
+# The INAs are attached to servos remote I2C bus.
+#
+
+# power_total
+#  5v_out
+#  3v3_out
+#  dvfs1
+#  gpu
+#  core
+#  dvfs2
+#  pmic
+#  dram
+#  sram15
+#  panelled
+#  panel_3v3
+#  sram7
+#  wifi_3v3
+#  ec_3v3
+#  ac_power
+
+inas = [(0x40, 'total', 19, 0.005, 'rem', True),
+        (0x41, '5v_out', 5, 0.005, 'rem', True),
+        (0x42, '3v3_out', 3.3, 0.005, 'rem', True),
+        (0x43, 'dvfs1', 1.8, 0.01, 'rem', True),
+        (0x44, 'gpu', 1.8, 0.02, 'rem', True),
+        (0x45, 'core', 5, 0.05, 'rem', True),
+        (0x46, 'dvfs2', 5, 0.02, 'rem', True),
+        (0x47, 'pmic', 1.35, 0.01, 'rem', True),
+        (0x48, 'dram', 1.35, 0.05, 'rem', True),
+        (0x49, 'sram15', 1.35, 0.1, 'rem', True),
+        (0x4A, 'panelled', 11.1, 0.05, 'rem', True),
+        (0x4B, 'panel_3v3', 3.3, 0.05, 'rem', True),
+        (0x4C, 'sram7', 5, 0.1, 'rem', True),
+        (0x4D, 'wifi_3v3', 1.8, 0.05, 'rem', True),
+        (0x4E, 'ec_3v3', 3.3, 0.1, 'rem', True),
+        (0x4F, 'ac_power', 3.3, 0.01, 'rem', True)]
