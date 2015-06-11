902f18105357e9058225f9bfcd0d653cf6bc7558
Todd Broch <tbroch@chromium.org>
2015-06-11 03:13:58 -0700

    glados: Enable current and power measurements using INA3221.
    BUG=chrome-os-partner:41410
    TEST=manual, emerges ... current/power values look reasonable.
    
    Change-Id: I26782150cb8e5619122bc1d7bcf3f41c95b06019
    Reviewed-on: https://chromium-review.googlesource.com/277704
    Reviewed-by: Alec Berg <alecaberg@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- servo/data/glados_p1_inas.py
+++ servo/data/glados_p1_inas.py
@@ -3,15 +3,15 @@
 # found in the LICENSE file.
 
 # These devices are ina3221 (3-channels/i2c address) devices
-inas = [('0x40:0', 'pp3300_dsw_ec',   3.3,   0.100, 'rem', False),
-        ('0x40:1', 'pp3300_dx_wlan',  3.3,   0.050, 'rem', False),
-        ('0x40:2', 'pp600_vtt',       0.6,   0.020, 'rem', False),
-        ('0x41:0', 'pp3300_dsw',      3.3,   0.100, 'rem', False),
-        ('0x41:1', 'pp5000_a',        5.0,   0.010, 'rem', False),
-        ('0x41:2', 'pp3300_a',        3.3,   0.020, 'rem', False),
-        ('0x42:0', 'pp1800_a',        1.8,   0.100, 'rem', False),
-        ('0x42:1', 'pp1800_dram',     1.8,   0.100, 'rem', False),
-        ('0x42:2', 'pp1200_vddq',     1.2,   0.005, 'rem', False),
-        ('0x43:0', 'pp1000_a',        1.0,   0.015, 'rem', False),
-        ('0x43:1', 'pp975_io',        0.975, 0.010, 'rem', False),
-        ('0x43:2', 'pp850_prim_core', 0.850, 0.015, 'rem', False)]
+inas = [('0x40:0', 'pp3300_dsw_ec',   3.3,   0.100, 'rem', True),
+        ('0x40:1', 'pp3300_dx_wlan',  3.3,   0.050, 'rem', True),
+        ('0x40:2', 'pp600_vtt',       0.6,   0.020, 'rem', True),
+        ('0x41:0', 'pp3300_dsw',      3.3,   0.100, 'rem', True),
+        ('0x41:1', 'pp5000_a',        5.0,   0.010, 'rem', True),
+        ('0x41:2', 'pp3300_a',        3.3,   0.020, 'rem', True),
+        ('0x42:0', 'pp1800_a',        1.8,   0.100, 'rem', True),
+        ('0x42:1', 'pp1800_dram',     1.8,   0.100, 'rem', True),
+        ('0x42:2', 'pp1200_vddq',     1.2,   0.005, 'rem', True),
+        ('0x43:0', 'pp1000_a',        1.0,   0.015, 'rem', True),
+        ('0x43:1', 'pp975_io',        0.975, 0.010, 'rem', True),
+        ('0x43:2', 'pp850_prim_core', 0.850, 0.015, 'rem', True)]
