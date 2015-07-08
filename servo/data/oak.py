d8a1c9e699f7999ec6d644754bfcec494e086137
Chung-yih Wang <cywang@chromium.org>
2015-07-08 15:29:45 +0800

    oak: update current sensing resistors for rev3
    BUG=chrome-os-partner:42201
    TEST=sudo emerge hdctools;
          sudo servod -c oak.xml &
          mw=`dut-control -i|grep _mw | cut -d ' ' -f1`
          dut-control $mw
    
    Change-Id: I64efb27ec974021ffa707b4c9096849a92e39649
    Reviewed-on: https://chromium-review.googlesource.com/283755
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Tony Lin <tonycwlin@google.com>
    Reviewed-by: Tony Lin <tonycwlin@google.com>
    Commit-Queue: Tony Lin <tonycwlin@google.com>

--- servo/data/oak.py
+++ servo/data/oak.py
@@ -23,19 +23,19 @@
 #  ec_3v3
 #  ac_power
 
-inas = [(0x40, 'total', 19, 0.005, 'rem', True),
-        (0x41, '5v_out', 5, 0.005, 'rem', True),
-        (0x42, '3v3_out', 3.3, 0.005, 'rem', True),
-        (0x43, 'dvfs1', 1.8, 0.01, 'rem', True),
-        (0x44, 'gpu', 1.8, 0.02, 'rem', True),
-        (0x45, 'core', 5, 0.05, 'rem', True),
-        (0x46, 'dvfs2', 5, 0.02, 'rem', True),
-        (0x47, 'pmic', 1.35, 0.01, 'rem', True),
-        (0x48, 'dram', 1.35, 0.05, 'rem', True),
-        (0x49, 'sram15', 1.35, 0.1, 'rem', True),
-        (0x4A, 'panelled', 11.1, 0.05, 'rem', True),
-        (0x4B, 'panel_3v3', 3.3, 0.05, 'rem', True),
+inas = [(0x40, 'total', 6, 0.01, 'rem', True),
+        (0x41, '5v_out', 5, 0.01, 'rem', True),
+        (0x42, '3v3_out', 3.3, 0.01, 'rem', True),
+        (0x43, 'dvfs1', 5, 0.01, 'rem', True),
+        (0x44, 'gpu', 5, 0.1, 'rem', True),
+        (0x45, 'core', 5, 0.1, 'rem', True),
+        (0x46, 'dvfs2', 5, 0.1, 'rem', True),
+        (0x47, 'pmic', 5, 0.01, 'rem', True),
+        (0x48, 'dram', 5, 0.1, 'rem', True),
+        (0x49, 'sram15', 5, 0.1, 'rem', True),
+        (0x4A, 'panelled', 11.1, 0.1, 'rem', True),
+        (0x4B, 'panel_3v3', 3.3, 0.1, 'rem', True),
         (0x4C, 'sram7', 5, 0.1, 'rem', True),
-        (0x4D, 'wifi_3v3', 1.8, 0.05, 'rem', True),
+        (0x4D, 'wifi_3v3', 3.3, 0.1, 'rem', True),
         (0x4E, 'ec_3v3', 3.3, 0.1, 'rem', True),
-        (0x4F, 'ac_power', 3.3, 0.01, 'rem', True)]
+        (0x4F, 'ac_power', 3.3, 0.1, 'rem', True)]
