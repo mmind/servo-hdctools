82795384a28e333400a1336c4a64f80027289de3
Derek Basehore <dbasehore@chromium.org>
2015-09-08 18:27:58 -0700

    glados: Add r2 inas
    This adds descriptions for the INAs on the glados r2 board. The sense
    resistor values are increased due to layout issues with vias. The value is
    based on comparing measurements between the INAs and a multimeter.
    
    BUG=None
    TEST=with servo connected to glados
    sudo emerge hdctools
    sudo servod -b glados -c glados_r2_inas.xml
    dut-control | grep _mw
    check that there are no errors
    
    Change-Id: Ib2e4c0df3a83287ea42bd8f68bb5ac4fe07037f0
    Reviewed-on: https://chromium-review.googlesource.com/298244
    Commit-Ready: Derek Basehore <dbasehore@chromium.org>
    Tested-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>

---
+++ servo/data/glados_r2_inas.py
@@ -0,0 +1,17 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# These devices are ina3221 (3-channels/i2c address) devices
+inas = [('0x40:0', 'pp3300_dsw_ec',   3.3,   0.100, 'rem', True),
+        ('0x40:1', 'pp3300_dx_wlan',  3.3,   0.057, 'rem', True),
+        ('0x40:2', 'pp3300_dx_edp',   3.3,   0.100, 'rem', True),
+        ('0x41:0', 'pp3300_dsw',      3.3,   0.100, 'rem', True),
+        ('0x41:1', 'pp5000_a',        5.0,   0.016, 'rem', True),
+        ('0x41:2', 'pp3300_a',        3.3,   0.01875, 'rem', True),
+        ('0x42:0', 'pp1800_a',        1.8,   0.100, 'rem', True),
+        ('0x42:1', 'pp1800_dram',     1.8,   0.100, 'rem', True),
+        ('0x42:2', 'pp1200_vddq',     1.2,   0.007, 'rem', True),
+        ('0x43:0', 'pp1000_a',        1.0,   0.01755, 'rem', True),
+        ('0x43:1', 'pp975_io',        0.975, 0.018, 'rem', True),
+        ('0x43:2', 'pp850_prim_core', 0.850, 0.016, 'rem', True)]
