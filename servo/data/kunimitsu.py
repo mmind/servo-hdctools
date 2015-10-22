b94ce71f767f577c463052ceb3a0c561ef3c3f0b
Roy Mingi Park <roy.mingi.park@intel.com>
2015-10-22 16:16:44 -0700

    kunimitsu: Add r2 inas
    This adds descriptions for the INAs on the kunimitsu r2 board. The sense
    resistor values are increased due to layout issues with vias. The value is
    based on comparing measurements between the INAs and a multimeter.
    
    BUG=None
    BRANCH=master
    TEST=with servo connected to kunimitsu
    	sudo emerge hdctools
    	sudo servod -b kunimitsu -c kunimitsu.xml
    	dut-control | grep _mw
    	check that there are no errors
    
    Change-Id: I926ba5371a53fd30b894ee89c2114db81934eac6
    Signed-off-by: Roy Mingi Park <roy.mingi.park@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/308224
    Commit-Ready: Kevin K Wong <kevin.k.wong@intel.com>
    Tested-by: Kevin K Wong <kevin.k.wong@intel.com>
    Reviewed-by: Todd Broch <tbroch@chromium.org>

---
+++ servo/data/kunimitsu.py
@@ -0,0 +1,14 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# These devices are ina219 devices
+inas = [('0x40', 'ppvccsa',         1.0,   0.002, 'rem', True),
+        ('0x41', 'pp1000_a',        1.0,   0.002, 'rem', True),
+        ('0x42', 'pp1000_vccstg',   1.0,   0.002, 'rem', True),
+        ('0x43', 'pp3300_dsw',      3.3,   0.005, 'rem', True),
+        ('0x47', 'ppina_vbat',      7.6,   0.020, 'rem', True),
+        ('0x49', 'pp1000_vcccore',  1.0,   0.002, 'rem', True),
+        ('0x4a', 'pp1000_vccgt',    1.0,   0.002, 'rem', True),
+        ('0x4b', 'pp5000_a',        5.0,   0.010, 'rem', True)
+        ]
