acdbe8f4fda15fba2ba7ef9ff678e6762ee927c6
tonycwlin <tonycwlin@google.com>
2016-02-01 16:27:22 +0800

    oak: update INA219 overlay to match Rev5 board configuration.
    (1) update power rails name
    (2) update sense resistors value
    
    BUG=None
    TEST=Manually
    $ sudo emerge hdctools
    $ sudo servod -b oak -c oak.xml
    
    Change-Id: Ida3f43151c1b5febac2a441af31b09bb31be280d
    Reviewed-on: https://chromium-review.googlesource.com/324679
    Commit-Ready: Tony Lin <tonycwlin@chromium.org>
    Tested-by: Tony Lin <tonycwlin@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>

--- servo/data/oak.py
+++ servo/data/oak.py
@@ -4,38 +4,20 @@
 
 # MTK OAK board sense resistors feed INA219s on the board.
 # The INAs are attached to servos remote I2C bus.
-#
 
-# power_total
-#  5v_out
-#  3v3_out
-#  dvfs1
-#  gpu
-#  core
-#  dvfs2
-#  pmic
-#  dram
-#  sram15
-#  panelled
-#  panel_3v3
-#  sram7
-#  wifi_3v3
-#  ec_3v3
-#  ac_power
-
-inas = [(0x40, 'battery', 6, 0.01, 'rem', True),
-        (0x41, '5v_out', 5, 0.01, 'rem', True),
+inas = [(0x40, 'battery', 7.4, 0.01, 'rem', True),
+        (0x41, '5v_out', 4.2, 0.01, 'rem', True),
         (0x42, '3v3_out', 3.3, 0.01, 'rem', True),
-        (0x43, 'dvfs1', 5, 0.01, 'rem', True),
-        (0x44, 'gpu', 5, 0.1, 'rem', True),
-        (0x45, 'core', 5, 0.1, 'rem', True),
-        (0x46, 'dvfs2', 5, 0.1, 'rem', True),
-        (0x47, 'pmic', 5, 0.01, 'rem', True),
-        (0x48, 'dram', 5, 0.1, 'rem', True),
-        (0x49, 'sram15', 5, 0.1, 'rem', True),
-        (0x4A, 'panelled', 11.1, 0.1, 'rem', True),
+        (0x43, 'dvfs1', 4.2, 0.01, 'rem', True),
+        (0x44, 'gpu', 4.2, 0.1, 'rem', True),
+        (0x45, 'core', 4.2, 0.1, 'rem', True),
+        (0x46, 'dvfs2', 4.2, 0.1, 'rem', True),
+        (0x47, 'pmic', 4.2, 0.01, 'rem', True),
+        (0x48, 'dram', 4.2, 0.1, 'rem', True),
+        (0x49, 'sram15', 4.2, 0.1, 'rem', True),
+        (0x4A, 'panelled', 7.4, 0.1, 'rem', True),
         (0x4B, 'panel_3v3', 3.3, 0.1, 'rem', True),
-        (0x4C, 'sram7', 5, 0.1, 'rem', True),
+        (0x4C, 'sram7', 4.2, 0.1, 'rem', True),
         (0x4D, 'wifi_3v3', 3.3, 0.1, 'rem', True),
-        (0x4E, 'ec_3v3', 3.3, 0.1, 'rem', True),
-        (0x4F, 'ac_power', 3.3, 0.01, 'rem', True)]
+        (0x4E, 'stby_3v3', 3.3, 0.1, 'rem', True),
+        (0x4F, 'ac_power', 20, 0.01, 'rem', True)]
