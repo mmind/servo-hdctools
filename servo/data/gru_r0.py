8ed184100e852e9880ecdc953973fc29279e11ee
Stephen Barber <smbarber@chromium.org>
2016-04-01 17:32:26 -0700

    gru: add revision 0 INA descriptors
    BUG=none
    TEST=use dut-control to query INAs
    
    Change-Id: I65fd9b7470cd9a22540719d2ec8c6a099b619395
    Reviewed-on: https://chromium-review.googlesource.com/336884
    Commit-Ready: Stephen Barber <smbarber@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Derek Basehore <dbasehore@chromium.org>

---
+++ servo/data/gru_r0.py
@@ -0,0 +1,22 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [
+        (0x40, 'pp5000', 5.0, 0.01, 'rem', True),
+        (0x41, 'ppvar_gpu', 0.9, 0.01, 'rem', True),
+        (0x42, 'pp3300_wifi_bt', 3.3, 0.01, 'rem', True),
+        (0x43, 'pp1500_ap_io', 1.5, 0.01, 'rem', True),
+        (0x44, 'pp3300', 3.3, 0.01, 'rem', True),
+        (0x45, 'ppvar_litcpu', 0.9, 0.01, 'rem', True),
+        (0x46, 'pp1800_s0', 1.8, 0.01, 'rem', True),
+        (0x47, 'pp3300_haven', 3.3, 0.01, 'rem', True),
+        (0x48, 'ppvar_bigcpu', 0.9, 0.01, 'rem', True),
+        (0x49, 'pp900_ap', 0.9, 0.01, 'rem', True),
+        (0x4A, 'pp1800_ec', 1.8, 0.01, 'rem', True),
+        (0x4B, 'pp1800_sensor', 1.8, 0.01, 'rem', True),
+        (0x4C, 'pp1800', 1.8, 0.01, 'rem', True),
+        (0x4D, 'pp1200_lpddr', 1.2, 0.01, 'rem', True),
+        (0x4E, 'pp3300_ec', 3.3, 0.01, 'rem', True),
+        (0x4F, 'pp3300_s0', 3.3, 0.01, 'rem', True),
+       ]
+
