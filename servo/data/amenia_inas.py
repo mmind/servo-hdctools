3601f99d986c69aac01bc573412ca5535955bff1
Todd Broch <tbroch@chromium.org>
2016-05-06 18:19:11 -0700

    amenia: add inas
    BUG=chrome-os-partner:53096
    TEST=sudo servod -c amenia_inas.xml
      for ctrl in `dut-control -i | grep _mv` ; do
        dut-control $ctrl
      done
    
    Voltages look correct.
    
    Change-Id: I3951960dff3698ec72e7358ca5b58128a22a3545
    Reviewed-on: https://chromium-review.googlesource.com/343223
    Commit-Ready: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>

---
+++ servo/data/amenia_inas.py
@@ -0,0 +1,12 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [('0x40', 'pp1050_s',    1.05, 0.010, 'rem', True),
+        ('0x41', 'pp1800_a',    1.80, 0.010, 'rem', True),
+        ('0x42', 'pp1200_vddq', 1.20, 0.010, 'rem', True),
+        ('0x43', 'pp3300_a',    3.30, 0.010, 'rem', True),
+        ('0x44', 'ppvbat',      7.50, 0.010, 'rem', True),
+        ('0x47', 'ppvccgi',     1.00, 0.010, 'rem', True),
+        ('0x49', 'ppvnn',       1.00, 0.010, 'rem', True),
+        ('0x4a', 'pp1240_a',    1.24, 0.010, 'rem', True),
+        ('0x4b', 'pp5000_a',    5.00, 0.010, 'rem', True)]
