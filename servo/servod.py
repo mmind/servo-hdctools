d9261efa52e82666efabb9d5c3c8988af664f9b8
Vincent Palatin <vpalatin@chromium.org>
2016-06-24 09:37:42 -0700

    servod: handle gracefully the lack of USB serial number
    All the callers assume that usb_get_iserial() is returning a string but
    it might return 'None' in case of error (e.g. no serial number on a
    device). So update the function to return an empty string rather than
    None to avoid dying in random places.
    The typical failing case was when running servod with '--serialname'
    parameter but no other USB filtering parameter, the daemon would iterate
    over all connected USB devices and call usb_get_iserial() for each of
    them. If one has no serial number, it would die like this when trying to
    apply a string function on 'None':
    Traceback (most recent call last):
      File "servod", line 9, in <module>
        load_entry_point('servo==0.0.1.post3721316', 'console_scripts', 'servod')()
      File "servo/servod.py", line 518, in main
        main_function()
      File "servo/servod.py", line 435, in main_function
        multiservo.parse_rc(logger, options.rcfile))
      File "servo/servod.py", line 328, in discover_servo
        all_servos.extend(usb_find(vid, pid, serialname))
      File "servo/servod.py", line 173, in usb_find
          (not serialname or usb_get_iserial(device).endswith(serialname)):
      AttributeError: 'NoneType' object has no attribute 'endswith'
    
    Actually, for devices without a serial number string, iSerialNumber is 0.
    So doing .getString() on 0 will likely return the string descriptor which
    contains non-ascii bytes and will end up triggering exception in various
    string conversion routines.
    Skip trying to get the string in this case.
    
    BUG=chrome-os-partner:54772
    TEST=run servod with just the '--serialname' parameter
    e.g. 'sudo servod --serial=00025' and see it starting up regardless
    of the random USB devices connected to the workstation.
    
    Change-Id: I0804dc06c64559f4772a31a90f9c32120c699294
    Reviewed-on: https://chromium-review.googlesource.com/356060
    Commit-Ready: Vincent Palatin <vpalatin@chromium.org>
    Tested-by: Vincent Palatin <vpalatin@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>

--- servo/servod.py
+++ servo/servod.py
@@ -131,10 +131,14 @@ def usb_get_iserial(device):
     device: usb.Device object
 
   Returns:
-    iserial: USB devices iSerial string
+    iserial: USB devices iSerial string or empty string if the device has
+             no serial number.
   """
   device_handle = device.open()
-  iserial = None
+  # The device has no serial number string descriptor.
+  if device.iSerialNumber == 0:
+    return ""
+  iserial = ""
   try:
     iserial = device_handle.getString(device.iSerialNumber, MAX_ISERIAL_STR)
   except usb.USBError, e:
@@ -142,13 +146,6 @@ def usb_get_iserial(device):
     #   usb.USBError: error sending control message: Broken pipe
     # Need to investigate further
     pass
-  except Exception, e:
-    # Servo micro and v4 currently do not support serial number.
-    if device.idVendor == 0x18d1 \
-       and device.idProduct in [0x501a, 0x501b, 0x500f]:
-      pass
-    else:
-      raise e
   return iserial
 
 def usb_find(vendor, product, serialname):
