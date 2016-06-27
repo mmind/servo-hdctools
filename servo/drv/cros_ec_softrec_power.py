f495d5d89d943b53b231c5b652d3a25b89debded
Kaiyen Chang <kaiyen.chang@intel.com>
2016-06-27 17:24:42 +0800

    power_state: Clear the recovery flags before power on in non-recovery mode
    Per Google's instruction, servod needs to ensure that the recovery flags
    are clear before power on in non-recovery mode. The power_state:rec flow
    will then set it if needed.
    
    BUG=chrome-os-partner:52143
    TEST=Pass platform_ServoPowerStateController with Wizpig
    
    Change-Id: I084aa5159487b3d53569e1c42021896891bdadc8
    Signed-off-by: Kaiyen Chang <kaiyen.chang@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/356244
    Reviewed-by: Richard Barnette <jrbarnette@google.com>
    Reviewed-by: Duncan Laurie <dlaurie@google.com>

--- servo/drv/cros_ec_softrec_power.py
+++ servo/drv/cros_ec_softrec_power.py
@@ -14,7 +14,9 @@ class crosEcSoftrecPower(cros_ec_power.CrosECPower):
   based board) require using the EC to trigger recovery mode.
   """
 
-  _HOSTEVENT_RECOVERY_CMD = dict(rec='hostevent set 0x4000',
+  _HOSTEVENT_RECOVERY_CMD = dict(rec_on='hostevent set 0x4000',
+                                 rec_off='hostevent clear 0x4000',
+                                 rec_off_clearb='hostevent clearb 0x4000',
                                  fastboot='hostevent set 0x1000000')
   # Time in seconds to allow the EC to pick up the recovery
   # host event.
@@ -24,7 +26,7 @@ class crosEcSoftrecPower(cros_ec_power.CrosECPower):
     """Power on the AP after initializing recovery state."""
     self._interface.power_short_press()
 
-  def _power_on_bytype(self, rec_mode, rec_type='rec'):
+  def _power_on_bytype(self, rec_mode, rec_type='rec_on'):
     if rec_mode == self.REC_ON:
       # Hold warm reset so the AP doesn't boot when EC reboots.
       self._interface.set('warm_reset', 'on')
@@ -39,14 +41,24 @@ class crosEcSoftrecPower(cros_ec_power.CrosECPower):
       # Now the EC keeps the AP off. Release warm reset before powering
       # on the AP.
       self._interface.set('warm_reset', 'off')
-      # ... and tell the EC to tell the CPU we're in recovery mode.
+    else:
+      # Need to clear the flag in secondary (B) copy of the host events if
+      # we're in non-recovery mode.
       self._interface.set('ec_uart_cmd',
-                          self._HOSTEVENT_RECOVERY_CMD[rec_type])
-      time.sleep(self._RECOVERY_DETECTION_DELAY)
+                          self._HOSTEVENT_RECOVERY_CMD['rec_off_clearb'])
+
+    # Tell the EC to tell the CPU we're in recovery mode or non-recovery mode.
+    self._interface.set('ec_uart_cmd', self._HOSTEVENT_RECOVERY_CMD[rec_type])
+    time.sleep(self._RECOVERY_DETECTION_DELAY)
     self._power_on_ap()
 
   def _power_on(self, rec_mode):
-    self._power_on_bytype(rec_mode)
+    if rec_mode == self.REC_ON:
+      rec_type='rec_on'
+    else:
+      rec_type='rec_off'
+
+    self._power_on_bytype(rec_mode, rec_type)
 
   def _power_on_fastboot(self):
     self._power_on_bytype(self.REC_ON, rec_type='fastboot')
