fe81ae172199c1fd22194000d3b1404ace23dbd7
Aseda Aboagye <aaboagye@google.com>
2016-08-02 09:14:12 -0700

    cros_ec_softrec_power: Clean up hostcmd dict.
    This commit cleans up the existing hostevent recovery command
    dictionary.  Previously, using the dict required that the strings be
    typed.  If they were mistyped, the interpreter would not flag it.
    Therefore, names are added for the recovery types along with their
    corresponding hostevent commands.
    
    BUG=None
    BRANCH=None
    TEST=verify platform_ServoPowerStateController passes for chell and
    cyan.
    
    Change-Id: I84455b2778ce03285bbcff59bc7c944176ea3b1c
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/365414
    Commit-Ready: Aseda Aboagye <aaboagye@chromium.org>
    Tested-by: Aseda Aboagye <aaboagye@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>

--- servo/drv/cros_ec_softrec_power.py
+++ servo/drv/cros_ec_softrec_power.py
@@ -13,11 +13,25 @@ class crosEcSoftrecPower(cros_ec_power.CrosECPower):
   A number of boards (generally, the ARM based boards and some x86
   based board) require using the EC to trigger recovery mode.
   """
+  # Recovery types.
+  _REC_TYPE_REC_ON = 'rec_on'
+  _REC_TYPE_REC_OFF = 'rec_off'
+  _REC_TYPE_REC_OFF_CLEARB = 'rec_off_clearb'
+  _REC_TYPE_FASTBOOT = 'fastboot'
 
-  _HOSTEVENT_RECOVERY_CMD = dict(rec_on='hostevent set 0x4000',
-                                 rec_off='hostevent clear 0x4000',
-                                 rec_off_clearb='hostevent clearb 0x4000',
-                                 fastboot='hostevent set 0x1000000')
+  # Corresponding hostevent commands entered on the EC.
+  _HOSTEVENT_CMD_REC_ON = 'hostevent set 0x4000'
+  _HOSTEVENT_CMD_REC_OFF = 'hostevent clear 0x4000'
+  _HOSTEVENT_CMD_REC_OFF_CLEARB = 'hostevent clearb 0x4000'
+  _HOSTEVENT_CMD_FASTBOOT = 'hostevent set 0x1000000'
+
+  _REC_TYPE_HOSTEVENT_CMD_DICT = {
+    _REC_TYPE_REC_ON : _HOSTEVENT_CMD_REC_ON,
+    _REC_TYPE_REC_OFF : _HOSTEVENT_CMD_REC_OFF,
+    _REC_TYPE_REC_OFF_CLEARB : _HOSTEVENT_CMD_REC_OFF_CLEARB,
+    _REC_TYPE_FASTBOOT : _HOSTEVENT_CMD_FASTBOOT
+  }
+
   # Time in seconds to allow the EC to pick up the recovery
   # host event.
   _RECOVERY_DETECTION_DELAY = 1
@@ -26,7 +40,7 @@ class crosEcSoftrecPower(cros_ec_power.CrosECPower):
     """Power on the AP after initializing recovery state."""
     self._interface.power_short_press()
 
-  def _power_on_bytype(self, rec_mode, rec_type='rec_on'):
+  def _power_on_bytype(self, rec_mode, rec_type=_REC_TYPE_REC_ON):
     if rec_mode == self.REC_ON:
       # Hold warm reset so the AP doesn't boot when EC reboots.
       self._interface.set('warm_reset', 'on')
@@ -44,21 +58,22 @@ class crosEcSoftrecPower(cros_ec_power.CrosECPower):
     else:
       # Need to clear the flag in secondary (B) copy of the host events if
       # we're in non-recovery mode.
-      self._interface.set('ec_uart_cmd',
-                          self._HOSTEVENT_RECOVERY_CMD['rec_off_clearb'])
+      cmd = self._REC_TYPE_HOSTEVENT_CMD_DICT[self._REC_TYPE_REC_OFF_CLEARB]
+      self._interface.set('ec_uart_cmd', cmd)
 
     # Tell the EC to tell the CPU we're in recovery mode or non-recovery mode.
-    self._interface.set('ec_uart_cmd', self._HOSTEVENT_RECOVERY_CMD[rec_type])
+    cmd = self._REC_TYPE_HOSTEVENT_CMD_DICT[rec_type]
+    self._interface.set('ec_uart_cmd', cmd)
     time.sleep(self._RECOVERY_DETECTION_DELAY)
     self._power_on_ap()
 
   def _power_on(self, rec_mode):
     if rec_mode == self.REC_ON:
-      rec_type='rec_on'
+      rec_type = self._REC_TYPE_REC_ON
     else:
-      rec_type='rec_off'
+      rec_type = self._REC_TYPE_REC_OFF
 
     self._power_on_bytype(rec_mode, rec_type)
 
   def _power_on_fastboot(self):
-    self._power_on_bytype(self.REC_ON, rec_type='fastboot')
+    self._power_on_bytype(self.REC_ON, rec_type=self._REC_TYPE_FASTBOOT)
